087e24b49d3e7a6ed98884cde31350b476f8a118
Vikas Yadav <vikas@digiscend.com>
2016-07-07 18:34:31 +0530

    floating nav bar started

--- app/src/main/java/com/digiscend/apps/browser/Activity/ProjectViewActivity.java
+++ app/src/main/java/com/digiscend/apps/browser/Activity/ProjectViewActivity.java
@@ -1,11 +1,17 @@
 package com.digiscend.apps.browser.Activity;
 
 import android.graphics.Bitmap;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,6 +24,7 @@ import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
 public class ProjectViewActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener
 {
 
     @Override
@@ -84,6 +91,52 @@ public class ProjectViewActivity extends AppCompatActivity
         {
             e.printStackTrace ();
         }
+
+        Toolbar toolbar = (Toolbar) findViewById (R.id.toolbar);
+        if(toolbar!=null)
+            setSupportActionBar (toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById (R.id.project_view_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle (
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener (toggle);
+        toggle.syncState ();
+
+        NavigationView navigationView = (NavigationView) findViewById (R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener (this);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        DrawerLayout drawer = (DrawerLayout) findViewById (R.id.project_view_layout);
+        if (drawer.isDrawerOpen (GravityCompat.START))
+        {
+            drawer.closeDrawer (GravityCompat.START);
+        } else
+        {
+            super.onBackPressed ();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item)
+    {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId ();
+
+        if (id == R.id.nav_camera)
+        {
+            // Handle the camera action
+        }  if (id == R.id.nav_share)
+        {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById (R.id.project_view_layout);
+        drawer.closeDrawer (GravityCompat.START);
+        return true;
     }
 
     // Inflate the menu; this adds items to the action bar if it is present.
